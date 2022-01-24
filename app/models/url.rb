ff9a298fc05902c1345971d1f9ea2e8b58b434bb
Igor Froehner <igoor.sf14@gmail.com>
2022-01-24 10:22:52 -0300

    feat: :lipstick: makers url shortener logo

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,10 +1,17 @@
 class Url < ApplicationRecord
   belongs_to :user
-  validates(:url,
+  validates :url,
             presence: true,
+            length: { maximum: 300, minimum: 5 },
             format: {
               with: URI::DEFAULT_PARSER.make_regexp,
               message: "URL Inválida",
-            })
-  validates :name, presence: true, uniqueness: true
+            }
+  validates :name,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 20, minimum: 3 },
+            exclusion: { in: %w([url new users]) },
+            format: { with: /\A\S+\Z/,
+                      message: "Apelido Inválido" }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :url, dependent: :destroy_async
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Email Inválido' }
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: { maximum: 150, minimum: 5 },
+            format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Email Inválido' }
 end
