23398d33743574b88c02985b94cbdb4fd75c6bfd
Igor Schiessl Froehner <igoor.sf14@gmail.com>
2023-01-24 18:22:16 -0300

    db: :seedling: user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create_or_find_by!(
+  email: 'user@email.com',
+  password: '1234'
+)
